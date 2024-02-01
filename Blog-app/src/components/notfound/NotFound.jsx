0f886de9d792dfe8aec1f32054a7f4d3d6d8124d
Sachin Gavali <sachingavali321@gmail.com>
2024-02-01 21:19:43 +0530

    just push

--- Blog-app/src/components/notfound/NotFound.jsx
+++ Blog-app/src/components/notfound/NotFound.jsx
@@ -1,7 +1,9 @@
 
 const NotFound = () => {
     return (
-        <div className="w-full h-screen bg-blue-700 text-3xl">NotFound</div>
+        <div className="w-full h-screen bg-blue-700 text-3xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet a facilis eos eaque est excepturi ipsa, itaque rem quia cum in placeat, praesentium repellendus laudantium ipsam amet magni! Officiis necessitatibus tempore debitis eligendi sed non magnam ipsam dolorem vel doloribus, nobis, tenetur beatae mollitia libero ullam, quaerat laudantium ratione quibusdam sapiente itaque possimus aut aliquam totam. Suscipit facere dolorum similique exercitationem ullam? Numquam est culpa minima repudiandae quasi accusamus, odio error asperiores dolor sapiente amet unde in laudantium quo veritatis obcaecati nesciunt sed provident. Error optio sequi magni quae quasi sunt officia a, consequatur nostrum quaerat tenetur, ut praesentium delectus!
+        </div>
     )
 }
 
