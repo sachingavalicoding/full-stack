52be7740b58a5c72a0ce772b303ff17c2a1aa776
Sachin Gavali <sachingavali321@gmail.com>
2024-02-08 20:05:03 +0530

    adding css

--- Database/src/App.jsx
+++ Database/src/App.jsx
@@ -1,35 +1,15 @@
-import { Client, Databases } from "appwrite";
-import { useEffect, useState } from "react";
+import { Client } from 'appwrite';
+
 const App = () => {
-  const [data, setData] = useState(null);
   const client = new Client();
 
-  const databases = new Databases(client);
   client
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('65a05a15e5700eeb68d5')
-    ;
-
-  const promise = databases.listDocuments('65b0455a93ac7df7644a', '65b045687ffabc2ab44a');
-
-  useEffect(() => {
-    promise.then(function (response) {
-      setData(response.documents) // Success
-    }, function (error) {
-      console.log(error); // Failure
-    });
-
-  }, [])
+    .setProject('65c1b9f230d4bdc30832');
 
   return (
-    <div className="text-xl text-green-600">
-      {
-        (data.map((item) => <h1 key={item.$id}> {item.name} {item.$id} {item.content} {item.email} </h1>))
-      }
-
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia cumque, cum sit odit debitis tenetur! Soluta et tenetur omnis nihil dicta deleniti distinctio. Doloremque id sequi quidem facere a aperiam quos distinctio cum, deleniti doloribus ratione laborum veritatis neque sit ipsum labore beatae corrupti magni necessitatibus consectetur ab? Eaque dicta quia velit id amet nostrum delectus deleniti accusamus dolore. Voluptatibus odit mollitia, fugit repellendus nisi similique vero cum magni dolorum quaerat accusamus, amet sed debitis itaque eveniet nostrum est eius inventore corporis maiores impedit laudantium eum quidem! Incidunt iure totam rerum, mollitia placeat labore itaque doloremque exercitationem, distinctio sit voluptas.
-      </p>
+    <div className="text-red-900">
+      Helllo
     </div>
   )
 }
---
+++ htmlcss/index.js
@@ -0,0 +1,46 @@
+// let i = 1;
+// do {
+//     console.log(i);
+//     i = i + 1;
+
+// } while (i <= 10);
+
+// // 2 
+// let i = 10;
+// let j = 10;
+// while (i > 0) {
+//     if (j % 2 != 0) {
+//         console.log(i - j);
+//         i--;
+//     }
+//     j--;
+// }
+
+
+// let i;
+// let j;
+// for (i = 10, j = 10; i >= 1; j--) {
+
+//     if (j % 2 != 0) {
+//         console.log(i - j);
+//         i--;
+//     }
+
+// }
+// let sum = 0;
+// for (let i = 0; i <= 10; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+// console.log((10 * (10 + 1)) / 2);
+
+
+// let str = "Sachin";
+// for (let i = 0; i < str.length; i++) {
+//     console.log(str[i]);
+// }
+
+let arr = [23, 455, 34, 5654, 45, 4];
+for (let i = 0; i < 6; i++) {
+    console.log(arr[i]);
+}
